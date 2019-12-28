f080b433d8a60c89ed63d6aa46fa8cbcad4efd81
Gethric <mangwisto@gmail.com>
2019-12-27 20:47:51 -0600

    Adds Sign Up form

--- src/components/SignUp/SignUp.component.jsx
+++ src/components/SignUp/SignUp.component.jsx
@@ -1,32 +1,62 @@
 import React from "react";
 import "./signup.styles.scss";
+import FormInput from "../FormInput/FormInput.component";
+import CustomButton from "../CustomButton/CustomButton.component";
 
-const SignUp = () => (
-	<div className="signup">
-		<div className="formwrap">
-			<div className="title">Sign-up</div>
-			<form id="signup">
-				<div className="fields_wrapper">
+class SignUp extends React.Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="signup">
+				<div className="title">Sign-up</div>
+				<form>
 					<div className="field">
-						<label for="name">Full Name</label>
-						<input type="text" id="name" />
+						<FormInput
+							name="fullName"
+							type="text"
+							label="Full Name"
+							onChange={this.handleChange}
+							required
+						/>
 					</div>
 					<div className="field">
-						<label for="email">Email Address</label>
-						<input type="text" id="email" />
+						<FormInput
+							name="email"
+							type="text"
+							label="Email"
+							onChange={this.handleChange}
+							required
+						/>
 					</div>
 					<div className="field">
-						<label for="password">Password</label>
-						<input type="password" id="password" />
+						<FormInput
+							name="password"
+							type="password"
+							label="Password"
+							onChange={this.handleChange}
+							required
+						/>
 					</div>
-				</div>
-				<div className="buttons">
-					<button id="inup">Sign Up</button>
-					<button id="change">Already have an account?</button>
-				</div>
-			</form>
-		</div>
-	</div>
-);
+					<div className="field">
+						<FormInput
+							name="confirmPassword"
+							type="password"
+							label="Confirm Password"
+							onChange={this.handleChange}
+							required
+						/>
+					</div>
+					<div className="buttons">
+						<CustomButton type="submit" id="up" label="Sign Up" />
+						<CustomButton id="change" label="Already have an account?" />
+					</div>
+				</form>
+			</div>
+		);
+	}
+}
 
 export default SignUp;
